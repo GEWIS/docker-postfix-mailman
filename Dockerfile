42da281b88095a7116b7adcd2f88cabc00399b95
rinkp <rink@pietershgv.nl>
2020-12-24 23:56:00 +0100

    Mailman config for postfix

--- Dockerfile
+++ Dockerfile
@@ -7,18 +7,13 @@ RUN apt-get update && \
     rm -rf /var/lib/apt/lists/* 
 
 # mail server will be listening on this port
-EXPOSE 25
+# EXPOSE 25
 
 # add configuration files
 ADD ./config/supervisord/supervisord.conf   /etc/supervisor/supervisord.conf
 ADD ./config/rsyslog/rsyslog.conf           /etc/rsyslog.conf
 ADD ./config/postfix/main.cf                /etc/postfix/main.cf
 ADD ./config/postfix/header_checks          /etc/postfix/header_checks
-ADD ./config/opendkim/opendkim.conf         /etc/opendkim.conf
-ADD ./config/opendkim/opendkim              /etc/default/opendkim
-ADD ./config/opendkim/TrustedHosts          /etc/opendkim/TrustedHosts
-ADD ./config/opendkim/SigningTable          /etc/opendkim/SigningTable
-ADD ./config/opendkim/KeyTable              /etc/opendkim/KeyTable
 
 # configure the entrypoint
 ADD ./docker-entrypoint.sh /docker-entrypoint.sh
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,24 +7,16 @@ if [ -z "${DOMAIN}" ]; then
     exit 1
 fi
 
-# TODO: escape domain!
-
-# replace the placeholders in the configuration files
-PATTERN="s/\${DOMAIN}/${DOMAIN}/g"
-sed -i ${PATTERN} /etc/postfix/main.cf
-sed -i ${PATTERN} /etc/opendkim/SigningTable
-sed -i ${PATTERN} /etc/opendkim/KeyTable
-
-# check the presence of the key for opendkim
-if [ ! -f /etc/opendkim/domainkeys/mail.private ]; then
-    echo "Cannot load the 'mail.private' file from '/etc/opendkim/domainkeys/mail.private'. Please mount it as a Docker volume before starting the container."
+# read relay from environment
+if [ -z "${RELAY}" ]; then
+    echo "RELAY environment variable not found. Please set it before running this Docker container."
     exit 1
 fi
 
-# fix permissions on files
-chown opendkim:opendkim /etc/opendkim/domainkeys
-chown opendkim:opendkim /etc/opendkim/domainkeys/mail.private
-chmod 400 /etc/opendkim/domainkeys/mail.private
+# replace the placeholders in the configuration files
+PATTERN="s/\${DOMAIN}/${DOMAIN}/g"
+PATTERN="s/\${RELAY}/${RELAY}/g"
+sed -i ${PATTERN} /etc/postfix/main.cf
 
 # launch the processes supervisor
 /usr/bin/supervisord -c /etc/supervisor/supervisord.conf
