3daf298d522488ad70e99b76ce0cff35272b43af
rinkp <rink@pietershgv.nl>
2020-12-25 14:47:03 +0100

    Require mynetworks to be set

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -13,10 +13,18 @@ if [ -z "${RELAY}" ]; then
     exit 1
 fi
 
+# read relay from environment
+if [ -z "${MYNETWORKS}" ]; then
+    echo "MYNETWORKS environment variable not found. Please set it before running this Docker container."
+    exit 1
+fi
+
 # replace the placeholders in the configuration files
 PATTERN="s/\${DOMAIN}/${DOMAIN}/g"
 sed -i ${PATTERN} /etc/postfix/main.cf
 PATTERN="s/\${RELAY}/${RELAY}/g"
+sed -i ${PATTERN} /etc/postfix/main.cf
+PATTERN="s/\${MYNETWORKS}/${MYNETWORKS}/g"
 sed -i ${PATTERN} /etc/postfix/main.cf
 
 # launch the processes supervisor
